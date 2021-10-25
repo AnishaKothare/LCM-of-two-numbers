d3ec9ab70f77d694b65a05147ff8867a8ba8c784
Megha Garg <43913641+miss-jain-16@users.noreply.github.com>
2021-10-25 15:43:24 +0530

    Update LCM.c
    Easy method by using gcd.

--- LCM.c
+++ LCM.c
@@ -1,33 +1,25 @@
 #include<stdio.h>
 
-int find_lcm(int, int);   // function prototype declaration
+  
 
 int main()
 {
     int a, b, lcm;
     printf("\n\nEnter 2 integers to find LCM of:\n");
     scanf("%d%d", &a, &b);
-    lcm = find_lcm(a,b);    // function call
-    printf("\n\n LCM of %d and %d is: %d\n\n", a, b, lcm);
+   // function call
+    printf("\n\n LCM of %d and %d is: %d\n\n", a, b, find_lcm(a,b);
     
     return 0;
 }
-
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
 int find_lcm(int a, int b)  // function definition
 {
-    /*
-        static variable is initialized only once 
-        for each function call
-    */
-    static int temp = 1;    
-    if(temp%a == 0 && temp%b == 0)
-    {
-        return temp;
-    }
-    else
-    {
-        temp++;
-        find_lcm(a,b);
-        return temp;
-    }
+   return (a / gcd(a, b)) * b;
+    
 }
